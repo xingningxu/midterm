cbd28c7fa8e3bfbabebb40428aa1a0d005d47175
Sheetal <mehtasheetal7987@gmail.com>
2019-02-11 08:26:55 -0500

    Initial Commit

---
+++ src/account/Account.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package account;
+
+/**
+ *
+ * @author sheetal
+ */
+public class Account {
+
+    
+        private double balance;
+        private double interestRate=0.23;
+ 
+     /**constructor that takes the initial balance
+      * @param initialBalance 
+      */
+        public Account(double initialBalance)
+        {
+               if (initialBalance > 0.0) 
+                   balance=initialBalance;
+        }
+
+     /**The method for depositing amount and 
+      * updating balance*/
+        public void credit(double amount)
+        {
+            balance=getBalance()+amount;
+        }
+
+     /**The method for debit(withdrawing amount) and 
+      * updating balance */
+        public void debit(double amount)
+        {
+            balance=getBalance()-amount;
+
+        }
+        
+     /**The getter for the balance
+     * @return the balance
+     */
+        public double getBalance() 
+        {
+        return balance;
+        }
+
+    /**The getter for InterestRate
+     * @return the interestRate
+     */
+        public double getInterestRate() 
+        {
+            return interestRate;
+        }
+    
+    
+}//class end
